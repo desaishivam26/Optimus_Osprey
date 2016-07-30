ce02e1667334469d9c0be616ba114cfe13656328
desaishivam26 <shivamdesaixda@gmail.com>
2016-07-30 14:01:25 +0530

    build: few adjustments

--- Anykernel2/ramdisk/init.optimus.sh
+++ Anykernel2/ramdisk/init.optimus.sh
@@ -10,15 +10,6 @@ echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control
 echo "bfq" > /sys/block/mmcblk0/queue/scheduler
 echo "512" > /sys/block/mmcblk0/bdi/read_ahead_kb
 
-#Interactive setup
-write /sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis 40000
-write /sys/devices/system/cpu/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif 1
-replace_line init.target.rc "write /sys/devices/system/cpu/cpufreq/interactive/min_sample_time 60000" "write /sys/devices/system/cpu/cpufreq/interactive/min_sample_time 40000"
-replace_line init.target.rc "write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 80" "write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 85"
-
 # CPU BOOST
 echo "40" > /sys/module/cpu_boost/parameters/input_boost_ms
 echo "1094400" > /sys/module/cpu_boost/parameters/input_boost_freq
