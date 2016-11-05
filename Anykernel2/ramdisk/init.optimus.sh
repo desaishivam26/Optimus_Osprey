8d80478a18d57d47a246605cf080920cd7eae5aa
desaishivam26 <shivamdesaixda@gmail.com>
2016-11-05 18:53:49 +0530

    Anykernel2: add cpufreq tunables

--- Anykernel2/ramdisk/init.optimus.sh
+++ Anykernel2/ramdisk/init.optimus.sh
@@ -10,3 +10,12 @@ echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control
 echo "bfq" > /sys/block/mmcblk0/queue/scheduler
 echo "512" > /sys/block/mmcblk0/bdi/read_ahead_kb
 
+# Cpufreq tuning
+echo "800000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo "20000" > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
+echo "60000" > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+echo "998400" > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+echo "80" > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+echo "20000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+echo "60000" > /sys/devices/system/cpu/cpufreq/interactive/boostpulse_duration
+echo "80" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
