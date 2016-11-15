4b2b85a270b2e047994f2a0d8f40227d5f97c27d
desaishivam26 <shivamdesaixda@gmail.com>
2016-11-15 09:09:58 +0530

    Anykernel: add UKSM & wakelocks config

--- Anykernel2/ramdisk/init.optimus.sh
+++ Anykernel2/ramdisk/init.optimus.sh
@@ -10,3 +10,9 @@ echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control
 echo "bfq" > /sys/block/mmcblk0/queue/scheduler
 echo "512" > /sys/block/mmcblk0/bdi/read_ahead_kb
 
+# Misc
+echo "0" > /sys/module/wakeup/parameters/enable_wlan_rx_wake_ws
+echo "0" > /sys/module/wakeup/parameters/enable_wlan_ctrl_wake_ws
+echo "0" > /sys/module/wakeup/parameters/enable_wlan_wake_ws
+echo "20" > /sys/kernel/mm/uksm/max_cpu_percentage
+echo "1" > /sys/kernel/mm/uksm/run
