b19776e0e97b4570b01a8323740daaeb1ad89e7e
desaishivam26 <shivamdesaixda@gmail.com>
2016-12-04 16:27:57 +0530

    Anykernel: disable UKSM

--- Anykernel2/ramdisk/init.optimus.sh
+++ Anykernel2/ramdisk/init.optimus.sh
@@ -15,4 +15,4 @@ echo "0" > /sys/module/wakeup/parameters/enable_wlan_rx_wake_ws
 echo "0" > /sys/module/wakeup/parameters/enable_wlan_ctrl_wake_ws
 echo "0" > /sys/module/wakeup/parameters/enable_wlan_wake_ws
 echo "20" > /sys/kernel/mm/uksm/max_cpu_percentage
-echo "1" > /sys/kernel/mm/uksm/run
+echo "0" > /sys/kernel/mm/uksm/run
