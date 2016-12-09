2c9443c69878627db0cef23c422242bd01ae61da
desaishivam26 <shivamdesaixda@gmail.com>
2016-12-09 23:16:18 +0530

    Anykernel: add process reclaim tweaks

--- Anykernel2/ramdisk/init.optimus.sh
+++ Anykernel2/ramdisk/init.optimus.sh
@@ -10,6 +10,13 @@ echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control
 echo "bfq" > /sys/block/mmcblk0/queue/scheduler
 echo "512" > /sys/block/mmcblk0/bdi/read_ahead_kb
 
+# Process Reclaim
+echo "1" > /sys/module/process_reclaim/parameters/enable_process_reclaim
+echo "50" > /sys/module/process_reclaim/parameters/pressure_min
+echo "70" > /sys/module/process_reclaim/parameters/pressure_max
+echo "512" > /sys/module/process_reclaim/parameters/per_swap_size
+echo "30" > /sys/module/process_reclaim/parameters/swap_opt_eff
+
 # Misc
 echo "0" > /sys/module/wakeup/parameters/enable_wlan_rx_wake_ws
 echo "0" > /sys/module/wakeup/parameters/enable_wlan_ctrl_wake_ws
