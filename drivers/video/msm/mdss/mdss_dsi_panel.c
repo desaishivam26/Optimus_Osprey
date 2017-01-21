57f04791e10de607d3613fd3866f6ac1522ff319
Joe Maples <joe@frap129.org>
2017-01-21 18:27:34 -0500

    lazyplug: Enter lazy through mdss
    Improve screen-off lazyness
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -82,6 +82,8 @@ static void mdss_dsi_panel_bl_on_defer_wait(struct mdss_dsi_ctrl_pdata *ctrl)
 	}
 }
 
+extern void lazyplug_enter_lazy(bool enter);
+
 bool display_on = true;
 
 bool is_display_on()
@@ -878,6 +880,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	}
 
 	display_on = true;
+	lazyplug_enter_lazy(false);
 
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
@@ -972,6 +975,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 
 	mdss_dsi_panel_off_in_prog_notify(pdata, pinfo);
 	display_on = false;
+	lazyplug_enter_lazy(true);
+
 
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_BLANK;
