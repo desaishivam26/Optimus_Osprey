a025d89b02f26d986cf28db5bb7f58fd648f9dda
desaishivam26 <shivamdesaixda@gmail.com>
2017-03-08 17:21:33 +0530

    build: Release 21

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ KERNEL_DEFCONFIG=osprey_defconfig
 DTBTOOL=$KERNEL_DIR/Dtbtool/
 JOBS=8
 ANY_KERNEL2_DIR=$KERNEL_DIR/Anykernel2/
-FINAL_KERNEL_ZIP=Optimus-R20-Osprey.zip
+FINAL_KERNEL_ZIP=Optimus-R21-Osprey.zip
 
 # Clean build always lol
 echo "**** Cleaning ****"
